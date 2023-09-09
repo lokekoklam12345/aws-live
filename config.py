03b2857069eac9cbc8bb86571925d4c20f9ea9f4
lokekoklam12345 <144537259+lokekoklam12345@users.noreply.github.com>
2023-09-09 21:31:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cbffvuek6hel.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lokekoklam-employee1"
+customregion = "us-east-1"
+

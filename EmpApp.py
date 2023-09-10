7392c83f999473a165f84bc355b00c27f6b5e710
lokekoklam12345 <144537259+lokekoklam12345@users.noreply.github.com>
2023-09-10 19:42:04 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('GetEmp.html')
+    return render_template('AddEmp.html')
 
 
 @app.route("/about", methods=['POST'])
@@ -31,56 +31,7 @@ def about():
     return render_template('www.tarc.edu.my')
 
 
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
-
-@app.route("/fetchdata", methods=['POST'])
+@app.route("/getemp", methods=['POST'])
 def GetEmp():
     emp_id = request.form['emp_id']
     select_sql = "SELECT * FROM employee WHERE emp_id = %s"
@@ -109,16 +60,15 @@ def GetEmp():
             "location": location
         }
 
-        return emp_details
-
+        return render_template('EditEmp.html',id=emp_id ,f_name=first_name, l_name=last_name , skill=pri_skill , loc=location )
     except Exception as e:
         return str(e)
 
     finally:
         cursor.close()
        
-     print("all modification done...")  
-     return render_template('EditEmp.html',id=emp_id ,f_name=first_name, l_name=last_name , skill=pri_skill , loc=location )
+    
+
 
 
 if __name__ == '__main__':

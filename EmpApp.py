26acf92ce35e0d573b9053e63da1c9a8a8943238
lokekoklam12345 <144537259+lokekoklam12345@users.noreply.github.com>
2023-09-10 20:14:17 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -82,6 +82,62 @@ def GetEmp():
     finally:
         cursor.close()
 
+@app.route("/editEmp", methods=['POST'])
+def UpdateEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
+
+    update_sql = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s, location=%s WHERE emp_id=%s"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+        # Check if the employee exists
+        check_sql = "SELECT * FROM employee WHERE emp_id = %s"
+        cursor.execute(check_sql, (emp_id,))
+        existing_employee = cursor.fetchone()
+
+        if not existing_employee:
+            return "Employee not found"
+
+        cursor.execute(update_sql, (first_name, last_name, pri_skill, location, emp_id))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+
+        # Update image file in S3
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data updated in MySQL RDS... updating image in S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location.get('LocationConstraint'))
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modifications done...")
+    return render_template('UpdateEmpOutput.html', name=emp_name)
 
     
 

5aa9c2bd9b4e8f346a250f1a234a6680ea735bde
lokekoklam12345 <144537259+lokekoklam12345@users.noreply.github.com>
2023-09-10 19:52:33 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -60,7 +60,7 @@ def GetEmp():
             "location": location
         }
 
-        return render_template('GetEmpOutput.html',id=emp_id ,f_name=first_name, l_name=last_name , skill=pri_skill , loc=location )
+        return render_template('GetEmpOutput.html',id=emp_id ,fname=first_name, lname=last_name , interest =pri_skill , location=location )
     except Exception as e:
         return str(e)
 

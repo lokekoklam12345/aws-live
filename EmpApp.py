cc93a9d6cc6b4c16e442c92985289e5d5e4f1e09
Kl0603 <lokekok47@gmail.com>
2023-09-10 23:39:57 +0800

    asd uoabcsou

--- EmpApp.py
+++ EmpApp.py
@@ -156,12 +156,12 @@ def LoginLec():
         email = request.form['email']
         password = request.form['password']
 
-        select_sql = "SELECT * FROM lecturer WHERE password = %s AND email = %s"
+        select_sql = "SELECT * FROM lecturer WHERE lectId = %s AND password = %s"
         cursor = db_conn.cursor()
 
 
         try:
-            cursor.execute(select_sql, (password,email,))
+            cursor.execute(select_sql, (email,password,))
             lecturer = cursor.fetchone()
 
             if not lecturer:

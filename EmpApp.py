f42f729b8ce61fd494212af110aa587716f5248c
lokekoklam12345 <144537259+lokekoklam12345@users.noreply.github.com>
2023-09-15 14:58:25 +0800

    ???

--- EmpApp.py
+++ EmpApp.py
@@ -912,38 +912,38 @@ def FilterCompany():
    
 
     try:
-            cursor.execute(select_sql)
-            companys = cursor.fetchall()  # Fetch all request
-                
-            company_list = []
+        cursor.execute(select_sql)
+        companys = cursor.fetchall()  # Fetch all request
+            
+        company_list = []
 
-            for companyExits in companys:
-                company_id = companyExits[0]
-                company_password = companyExits[1]
-                company_name = companyExits[2]
-                company_about = companyExits[3]
-                company_address = companyExits[4]
-                company_email= companyExits[5]
-                company_phone= companyExits[6]            
+        for companyExits in companys:
+            company_id = companyExits[0]
+            company_password = companyExits[1]
+            company_name = companyExits[2]
+            company_about = companyExits[3]
+            company_address = companyExits[4]
+            company_email= companyExits[5]
+            company_phone= companyExits[6]            
 
-                try:                
-                    company_data = {
-                        "id": company_id,
-                        "password": company_password,
-                        "name": company_name,
-                        "about": company_about,
-                        "address": company_address,
-                        "email": company_email,
-                        "phone": company_phone,
-                        
-                    }
-
-                    # Append the student's dictionary to the student_list
-                    company_list.append(company_data)
+            try:                
+                company_data = {
+                    "id": company_id,
+                    "password": company_password,
+                    "name": company_name,
+                    "about": company_about,
+                    "address": company_address,
+                    "email": company_email,
+                    "phone": company_phone,
                     
+                }
 
-                except Exception as e:
-                    return str(e)               
+                # Append the student's dictionary to the student_list
+                company_list.append(company_data)
+                
+
+            except Exception as e:
+                return str(e)               
 
         
         except Exception as e:
